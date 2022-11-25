394fbd068608d5e026b92c032d094800bb75b743
badaev52pk1 <badaev52@gmail.com>
2022-11-25 18:50:09 +0300

    bug fix exercise1

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine();
 
 Console.Write("Введите число 2: ");
 str2 = Console.ReadLine();
-Console.WriteLine("Вы ввели число: " + str1);
+Console.WriteLine("Вы ввели число: " + str2);
 
 Console.WriteLine();
 
@@ -19,9 +19,9 @@ num1 = Convert.ToInt32(str1);
 num2 = Convert.ToInt32(str2);
 
 if(num1 > num2)
-    Console.WriteLine("Число 1: " + num1 + " больше " + " Числа 2: " + num2);
+    Console.WriteLine(num1 + " больше " + num2);
 else if(num2 > num1)
-    Console.WriteLine("Число 2: " + num2 + " больше " + " Числа 1: " + num1);
+    Console.WriteLine(num2 + " больше " + num1);
 else
-    Console.WriteLine("Число 1: " + num1 + " равно " + " Числу 2: " + num2);
+    Console.WriteLine(num1 + " равно " + num2);
 
--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? str1;
+string? str2;
+string? str3;
+
+int num1, num2, num3;
+
+Console.Write("Введите число 1: ");
+str1 = Console.ReadLine();
+Console.WriteLine("Вы ввели число: " + str1);
+Console.WriteLine();
+
+Console.Write("Введите число 2: ");
+str2 = Console.ReadLine();
+Console.WriteLine("Вы ввели число: " + str2);
+Console.WriteLine();
+
+Console.Write("Введите число 3: ");
+str2 = Console.ReadLine();
+Console.WriteLine("Вы ввели число: " + str3);
+Console.WriteLine();
+
+num1 = Convert.ToInt32(str1);
+num2 = Convert.ToInt32(str2);
