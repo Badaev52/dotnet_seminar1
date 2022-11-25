b5888236fc6fc65125ad2737733f05c8a53730e5
badaev52pk1 <badaev52@gmail.com>
2022-11-25 19:28:24 +0300

    code ready exercise4

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -6,3 +6,13 @@ str = Console.ReadLine();
 Console.WriteLine();
 
 num = Convert.ToInt32(str);
+
+int count = 1;
+
+while (count <= num){
+    int n = count;
+    if ((n % 2) == 0){
+        Console.Write(n + ", ");
+    }
+    count++;
+}
