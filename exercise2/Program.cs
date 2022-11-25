8238436b4ad360f1f53737211e6492069fcac3b6
badaev52pk1 <badaev52@gmail.com>
2022-11-25 18:56:52 +0300

    code ready exercise2

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -3,6 +3,7 @@ string? str2;
 string? str3;
 
 int num1, num2, num3;
+int max;
 
 Console.Write("Введите число 1: ");
 str1 = Console.ReadLine();
@@ -15,9 +16,19 @@ Console.WriteLine("Вы ввели число: " + str2);
 Console.WriteLine();
 
 Console.Write("Введите число 3: ");
-str2 = Console.ReadLine();
+str3 = Console.ReadLine();
 Console.WriteLine("Вы ввели число: " + str3);
 Console.WriteLine();
 
 num1 = Convert.ToInt32(str1);
 num2 = Convert.ToInt32(str2);
+num3 = Convert.ToInt32(str3);
+
+max = num1;
+
+if (num2 > max)
+    max = num2;
+if (num3 > max)
+    max = num3;
+
+Console.WriteLine("Максимальное число : " + max);
