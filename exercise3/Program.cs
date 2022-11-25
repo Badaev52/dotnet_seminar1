8652a1d63810a7c9c27c6c7eb79fe48cbbeafa94
badaev52pk1 <badaev52@gmail.com>
2022-11-25 19:11:19 +0300

    code ready exercise3

--- exercise3/Program.cs
+++ exercise3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? str;
+int num;
+
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+Console.WriteLine();
+
+num = Convert.ToInt32(str);
+
+if ( num == 0)
+    Console.WriteLine("Число " + num + " равно нулю");
+else if ((num % 2) == 0)
+    Console.WriteLine("Число " + num + " является четным");
+else
+    Console.WriteLine("Число " + num + " не является четным");
