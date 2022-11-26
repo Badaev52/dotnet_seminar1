19367126cc22d0549204a5605454d51617f12413
badaev52pk1 <badaev52@gmail.com>
2022-11-26 15:42:23 +0300

    end

--- exercise21/Program.cs
+++ exercise21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? str;
+int num;
+
+Console.Write("Введите число: ");
+str = Console.ReadLine();
+Console.WriteLine();
+
+num = Convert.ToInt32(str);
+
+num = num / 10;
+num = num % 10;
+
+// num = num % 100;
+// num = num / 10;
+
+Console.WriteLine(num);
